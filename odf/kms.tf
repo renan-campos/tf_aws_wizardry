17d4522755577df51cb6c93d3f0a9b8632e85daf
kbader <kbader@redhat.com>
2022-04-28 14:34:22 -0700

    add kms alias

--- odf/kms.tf
+++ odf/kms.tf
@@ -3,3 +3,8 @@ resource "aws_kms_key" "odf" {
   key_usage   = "SIGN_VERIFY"
   customer_master_key_spec = "RSA_4096"
 }  
+
+resource "aws_kms_alias" "odf_alias" {
+  name = "alias/odf"
+  target_key_id = aws_kms_key.odf.key_id
+}
